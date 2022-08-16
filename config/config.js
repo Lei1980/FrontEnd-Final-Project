5283d5d4410796a822a9af78e65198ae9cb563b0
Lei Wang <jian.wang@realb.xyz>
2022-08-16 16:32:37 -0400

    Final Commit

---
+++ config/config.js
@@ -0,0 +1,76 @@
+var config = {
+    name: "Lei Wang",
+    email: "wanglei19801977@gmail.com",
+    address: "Montreal- a beautiful city",
+    log: "Happy developer",
+    excpect_work: "Java/ Javascript",
+
+    motto: [
+        "Tomorrow may not be better, but we must firmly believe that a better tomorrow will come"
+    ],
+    welcome: "Hi, this is Lei' home page, a vanier college student<br>" +
+             "Nice to meet you!",
+
+    about: "<p>I'm lei Wang, a student in web in Vanier college。I'm looking for a position in Java/ Javascript</p>" +
+        "<p>Looking forward to talking to you!</p>",
+ 
+    skills: [
+        ["Java", 80, "red"],
+        ["Php", 77, "blue"],
+        ["SQL", 75, "#1abc9c"],
+        ["HTML5", 67, "rgba(0,0,0)"],
+        ["CSS3", 60, "yellow"],
+        ["JavaScript", 70, "pink"]
+    ],
+
+    skills_description: "<ul>" +
+        "     <li>Proficient in OS and network</li>" +
+        "     <li>Proficient in core Java</li>" +
+        "     <li>Proficient in Java IO , multithread, Colletions</li>" +
+        "     <li>Basic understand JVM</li>" +
+        "     <li>Proficient in Php</li>" +
+        "     <li>Proficient in DB and SQL</li>" +
+        "     <li>Proficient in Linux</li>" +
+        "     <li>Proficient in Spring and Springboot</li>" +
+        "     <li>Proficient in Cache and MQ</li>" +
+        "     <li>Proficient in Distribution system</li>" +
+        "     <li>Proficient in Frontend</li>" +
+        " </ul>",
+
+    portfolio: [
+        ["./images/pro-1.png", "https://www.realb.xyz/", "Startup Project", "It's a property sell and buy built on Web3.0"],
+        ["./images/pro-1.png", "https://www.realb.xyz/", "Startup Project", "It's a property sell and buy built on Web3.0"],
+        ["./images/pro-1.png", "https://www.realb.xyz/", "Startup Project", "It's a property sell and buy built on Web3.0"]
+    ],
+
+    work: [
+
+        ["2006/7/1 — 2015/10/10", "<br>Teacher in High School",
+            "<p>Teaching all cours and schedule</p>" +
+            "<p>Manage the whole grade</p>"
+        ],
+
+        ["2022/1/1 — Present", "<br>Student in Vanier college",
+            "<p><strong>Study web technology</strong></p>"
+        ]
+    ],
+
+    others: [
+        ["2016-2018", "Waiting for my second baby", "All good"],
+        ["2018-2021", "Thinking about my future", "It's time to think about my future"],
+        ["2022", "I know what I will do", "Starting my dream"]
+    ],
+
+    icon: [
+        ["./svg/github.svg", "https://github.com/Lei1980", "My Github"]
+    ],
+
+
+    url: [
+        "./images/intro-bg.jpg",
+        "./images/2.jpg",
+        "./images/work-bk.png",
+        "./images/4.jpg"
+    ]
+
+}
